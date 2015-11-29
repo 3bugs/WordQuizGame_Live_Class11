2cc98e311e7a2aa8d6ba8f68a9b90d812892f5ba
Promlert Lovichit <promlert@gmail.com>
2015-11-29 10:24:01 +0700

    Add list dialog to choose difficulty level

--- app/src/main/java/com/example/wordquizgame/MainActivity.java
+++ app/src/main/java/com/example/wordquizgame/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.wordquizgame;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private Button playGameButton, highScoreButton;
 
     @Override
@@ -22,10 +26,26 @@ public class MainActivity extends AppCompatActivity {
         playGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle(R.string.choose_diff_label);
+
+                String[] diffLabels = new String[]{"ง่าย", "ปานกลาง", "ยาก"};
+
+                dialog.setItems(diffLabels, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i(TAG, "คุณเลือก: " + which);
+                    }
+                });
+
+                dialog.show();
+/*
                 Intent i = new Intent(MainActivity.this, GameActivity.class);
                 i.putExtra("user", "Promlert");
                 i.putExtra("age", 41);
                 startActivity(i);
+*/
             }
         });
 
